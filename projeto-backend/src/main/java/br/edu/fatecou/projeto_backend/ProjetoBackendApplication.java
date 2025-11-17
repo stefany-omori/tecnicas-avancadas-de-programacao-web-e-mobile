1730362899d7a65b080b8f7e97e7b19853dd6bbb
Stefany Kaori Omori <163460921+stefany-omori@users.noreply.github.com>
2025-11-17 13:44:08 +0000

    criação do projeto

---
+++ projeto-backend/src/main/java/br/edu/fatecou/projeto_backend/ProjetoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecou.projeto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackendApplication.class, args);
+	}
+
+}
---
+++ projeto-backend/src/test/java/br/edu/fatecou/projeto_backend/ProjetoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecou.projeto_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
